d3c2a80ca69faf093416d0bb12536ddfba254316
anichukwuebuka <anichukwuebuka65@gmail.com>
2022-12-18 23:08:46 +0000

    some changes

--- src/pages_components/PostWithComments.jsx
+++ src/pages_components/PostWithComments.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
-import Comments from '../components/postDetails/comments'
-import PostBody from '../components/postDetails/postBody'
+import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import Comments from "../components/postDetails/comments";
+import PostBody from "../components/postDetails/postBody";
 
 const PostWithComments = () => {
-    const {id} = useParams()
-    const post = useSelector(state => state.posts.posts.find(post => post.id === parseInt(id)))
-    
-  return (
-    <div className = 'col-span-2 w-full sm:w-2/3 md:w-full lg:w-4/5 md:pl-3 mx-auto pt-0 pb-4 mb-4 md:overflow-auto '>
-        <PostBody post={post}/>
-        <Comments id={id}/>
-    </div>
-  )
-}
+  const { id } = useParams();
+  const post = useSelector((state) =>
+    state.posts.posts.find((post) => post.id === parseInt(id))
+  );
 
-export default PostWithComments
+  return (
+    <div className="col-span-2 w-full sm:w-2/3 md:w-full lg:w-4/5 md:pl-3 mx-auto pt-0 pb-4 mb-4 md:overflow-y-scroll ">
+      <PostBody post={post} />
+      <Comments id={id} />
+    </div>
+  );
+};
+
+export default PostWithComments;
